d766ab1f2bb10bfbad799eb73536061aa955279f
Raouf213 <77270661+Raouf213@users.noreply.github.com>
2021-07-08 12:58:30 +0100

    Add files via upload

---
+++ faq-accordion-card-main/app.js
@@ -0,0 +1,111 @@
+var question1 = document.getElementById('p1');
+var slide = document.getElementById('slide');
+var down = document.getElementById('down');
+var button1 = document.getElementById('ask1');
+var h1 = document.getElementById('h1');
+
+
+up = () =>{
+    if ((question1.style.display) === 'none'){
+        question1.style.display = "flex";
+        button1.style.transform = "rotatex(180deg)";
+        button1.style.transition = "1s ease all";
+        h1.style.color ="hsl(238, 29%, 16%)";
+        h1.style.fontWeight ="700";
+    } else{
+        question1.style.display = "none";
+        button1.style.transform = "rotate(0deg)";
+        h1.style.color ="hsl(238, 29%, 16%)";
+        h1.style.fontWeight ="400";
+    }
+}
+
+
+
+
+// question 2 ===>
+
+var question2 = document.getElementById('p2');
+var button2 = document.getElementById('ask2');
+var h2 = document.getElementById('h2');
+
+up1 = () =>{
+    if ((question2.style.display) === 'none'){
+        question2.style.display = "flex";
+        button2.style.transform = "rotatex(180deg)";
+        button2.style.transition = "1s ease all";
+        h2.style.color ="hsl(238, 29%, 16%)";
+        h2.style.fontWeight ="700";
+    } else{
+        question2.style.display = "none";
+        button2.style.transform = "rotate(0deg)";
+        h2.style.color ="hsl(238, 29%, 16%)";
+        h2.style.fontWeight ="400";
+    }
+}
+
+
+// question 3 ===>
+
+var question3 = document.getElementById('p3');
+var button3 = document.getElementById('ask3');
+var h3 = document.getElementById('h3');
+
+up2 = () =>{
+    if ((question3.style.display) === 'none'){
+        question3.style.display = "flex";
+        button3.style.transform = "rotatex(180deg)";
+        button3.style.transition = "1s ease all";
+        h3.style.color ="hsl(238, 29%, 16%)";
+        h3.style.fontWeight ="700";
+    } else{
+        question3.style.display = "none";
+        button3.style.transform = "rotate(0deg)";
+        h3.style.color ="hsl(238, 29%, 16%)";
+        h3.style.fontWeight ="400";
+    }
+}
+
+
+// question 4 ===>
+
+var question4 = document.getElementById('p4');
+var button4 = document.getElementById('ask4');
+var h4 = document.getElementById('h4');
+
+up3 = () =>{
+    if ((question4.style.display) === 'none'){
+        question4.style.display = "flex";
+        button4.style.transform = "rotatex(180deg)";
+        button4.style.transition = "1s ease all";
+        h4.style.color ="hsl(238, 29%, 16%)";
+        h4.style.fontWeight ="700";
+    } else{
+        question4.style.display = "none";
+        button4.style.transform = "rotate(0deg)";
+        h4.style.color ="hsl(238, 29%, 16%)";
+        h4.style.fontWeight ="400";
+    }
+}
+
+// question 5 ===>
+
+var question5 = document.getElementById('p5');
+var button5 = document.getElementById('ask5');
+var h5 = document.getElementById('h5');
+
+up4 = () =>{
+    if ((question5.style.display) === 'none'){
+        question5.style.display = "flex";
+        button5.style.transform = "rotatex(180deg)";
+        button5.style.transition = "1s ease all";
+        h5.style.color ="hsl(238, 29%, 16%)";
+        h5.style.fontWeight ="700";
+    } else{
+        question5.style.display = "none";
+        button5.style.transform = "rotate(0deg)";
+        h5.style.color ="hsl(238, 29%, 16%)";
+        h5.style.fontWeight ="400";
+    }
+}
+
